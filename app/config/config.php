0d6bd10d28316e0c2e1b8fae18c4ffd4a653c4dd
Linasze <zykas911@gmail.com>
2018-07-23 11:57:58 +0300

    Add config

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '******');
+define('DB_NAME', 'meal');
+
+// APP ROOT
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'http://localhost/meal');
+
+//Site Name
+define('SITENAME', 'Meals nutrition calculator');
