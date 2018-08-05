4a2e2ab2838c92ab05a750a0d3b3442392dfc8b7
Linasze <zykas911@gmail.com>
2018-08-05 15:33:54 +0300

    Add  dash.js file

--- app/views/theme/footer.php
+++ app/views/theme/footer.php
@@ -9,6 +9,8 @@
     <script src="https://getbootstrap.com/docs/4.1/assets/js/vendor/popper.min.js"></script>
     <script src="https://getbootstrap.com/docs/4.1/dist/js/bootstrap.min.js"></script>
     <script src="<?php echo URLROOT; ?>/timepicker/timepicker.js"></script>
+    <script src="<?php echo URLROOT; ?>/js/dash.js"></script>
+
  <!-- Icons -->
     <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
     <script>
