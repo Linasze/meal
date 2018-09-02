8b2a90dc793cc18db948336483c9b195376e48cc
Linasze <zykas911@gmail.com>
2018-09-02 12:03:16 +0300

    Change Regenerate plan button place

--- app/views/settings/planSets.php
+++ app/views/settings/planSets.php
@@ -1,59 +1,61 @@
 <?php require_once APPROOT . '/views/theme/header.php'; ?>
 <?php flash('update'); ?>
 <div class="container">
-	<div class="row">
+    <div class="row">
 
 <div class="col-md-9 col-xs mx-auto">
   <div class="card">
 <div class="card-body">
     <div class="row">
-        <div class="col-md">
-            <h4>Nutrition plan settings</h4>
-            <hr>
-        </div>
+        <div class="col-md-5"><h4>Nutrition plan settings</h4></div>
+            <div class="col-md-7">
+<form action="<?php echo URLROOT;?>/dashboards/regenerate" method="POST" class="btn-floating">
+<input type="submit" class="btn btn-secondary" value="Regenerate Plan">
+</form></div>
     </div>
+    <hr>
     <div class="row">
         <div class="col-md col-xs">
   <form action="updatePlan" method="post">
         <div class="form-group row">
-          <label class="col-4 col-form-label">Wake up time</label> 
+          <label class="col-4 col-form-label">Wake up time</label>
           <div class="col-8">
             <input id="wake_up" name="wake_up"  class="form-control here" required="required" type="text" value="<?php echo $data['settings']->wake_up;?>">
           </div>
         </div>
         <div class="form-group row">
-          <label  class="col-4 col-form-label">Go to sleep time</label> 
+          <label  class="col-4 col-form-label">Go to sleep time</label>
           <div class="col-8">
             <input id="go_sleep" name="go_sleep" required="required" class="form-control here" type="text" value="<?php echo $data['settings']->go_sleep;?>">
           </div>
         </div>
         <div class="form-group row">
-          <label  class="col-4 col-form-label">Eatings count per day</label> 
+          <label  class="col-4 col-form-label">Eatings count per day</label>
           <div class="col-8">
           <select class="form-control" name="eating_count">
                 <?php for($i = 3; $i <= 6; $i++): ?>
           <option class="form-control" value="<?php echo $i; ?>"<?php if($data['settings']->eating_count == $i){?> selected <?php };?>> <?php echo $i; ?></option>
-                <?php endfor;?>   
+                <?php endfor;?>
           </select>
           </div>
         </div>
         <div class="form-group row">
-          <label class="col-4 col-form-label">Kilocalories</label> 
+          <label class="col-4 col-form-label">Kilocalories</label>
           <div class="col-8">
-  <?php 
+  <?php
      $age = (date("md", date("U", mktime(0, 0, 0, $data['user']->month, $data['user']->day, $data['user']->years))) > date("md")? ((date("Y") - $data['user']->years) - 1) : (date("Y") - $data['user']->years));
    ?>
     <input id="kcal" name="kcal" class="form-control here" required="required" type="text" value="<?php
   if($data['user']->sex == 1){
 echo round((66.5 + (13.75 * $data['user']->weight) + (5.003 * $data['user']->height) - (6.755 * $age)) * $data['user']->activity + $data['user']->purpose);
-}else{ 
+}else{
 echo round((655.1 + (9.563 * $data['user']->weight) + (1.850 * $data['user']->height) - (4.676 * $age)) * $data['user']->activity + $data['user']->purpose);
  } ?>" disabled>
           </div>
         </div>
-        
+
         <div class="form-group row">
-          <label  class="col-4 col-form-label">Physical activity</label> 
+          <label  class="col-4 col-form-label">Physical activity</label>
           <div class="col-8">
           <select name="activity" id="activity" class="form-control">
             <option class="form-control" value="1.2"<?php if($data['user']->activity == 1.2){?> selected <?php };?>>Sedentary work and low physical activity.</option>
@@ -65,7 +67,7 @@ echo round((655.1 + (9.563 * $data['user']->weight) + (1.850 * $data['user']->he
           </div>
         </div>
             <div class="form-group row">
-          <label  class="col-4 col-form-label">Purpose</label> 
+          <label  class="col-4 col-form-label">Purpose</label>
           <div class="col-8">
             <select name="purpose" id="purpose" class="form-control">
             <option class="form-control" value="0"<?php if($data['user']->purpose == 0){?> selected <?php };?>>To live healthier, and get more fit.</option>
@@ -82,10 +84,10 @@ echo round((655.1 + (9.563 * $data['user']->weight) + (1.850 * $data['user']->he
       </form>
         </div>
     </div>
-		        </div>
-		    </div>
-		</div>
-	</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <?php require_once APPROOT . '/views/theme/footer.php'; ?>
 
